56dc7e659535aa6462c79c3f5578ac3755c600ea
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-02-28 08:52:58 -0800

    Project Files are in.

---
+++ Source/CompWornFromInventory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace GearUpAndGo
+{
+    public class CompWornFromInventory : ThingComp
+    {
+        private HashSet<Thing> WornFromInventory = new HashSet<Thing>();
+        
+        public HashSet<Thing> GetHashSet()
+        {
+            return WornFromInventory;
+        }
+
+        public void RegisterWornItem(Thing thing)
+        {
+            this.WornFromInventory.Add(thing);
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Collections.Look<Thing>(ref this.WornFromInventory, "ThingsWornFromInventory", LookMode.Reference);
+        }
+    }
+}
--- Source/GearUpAndGo.cs
+++ Source/GearUpAndGo.cs
@@ -1,6 +1,34 @@
-﻿namespace GearUpAndGo
+﻿using System.Reflection;
+using Verse;
+using Harmony;
+
+namespace GearUpAndGo
 {
     public class GearUpAndGo
     {
+        public class Mod : Verse.Mod
+        {
+            public Mod(ModContentPack content) : base(content)
+            {
+                //// initialize settings
+                //GetSettings<Settings>();
+#if DEBUG
+                HarmonyInstance.DEBUG = true;
+#endif
+                HarmonyInstance harmony = HarmonyInstance.Create("uuugggg.rimworld.GearUpAndGo.main");
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+
+            //public override void DoSettingsWindowContents(Rect inRect)
+            //{
+            //    base.DoSettingsWindowContents(inRect);
+            //    GetSettings<Settings>().DoWindowContents(inRect);
+            //}
+
+            //public override string SettingsCategory()
+            //{
+            //    return "Gear Up And Go";
+            //}
+        }
     }
 }
