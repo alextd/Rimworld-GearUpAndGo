f5a2db6b01b81d61befee7aeb93d19bc023c6288
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-10-26 18:53:21 -0700

    An alert indicating we've set the Better Pawn Control Policy. Click it to reset.

---
+++ Source/Alert_GearedUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace GearUpAndGo
+{
+	public class Alert_GearedUp : Alert
+	{
+		public Alert_GearedUp()
+		{
+			this.defaultExplanation = "Gear Up And Go has set Better Pawn Control Policy for a battle.\n\nClick to reset to previous policy" ;
+		}
+
+		public override AlertReport GetReport()
+		{
+			GearUpPolicyComp comp = Current.Game.GetComponent<GearUpPolicyComp>();
+
+			if (comp.lastPolicy == "") return false;
+
+			return true;
+		}
+
+		private const float Padding = 6f;
+		public override Rect DrawAt(float topY, bool minimized)
+		{
+			//float height = TexGearUpAndGo.guagIconActive.height;	//The read out really doesn't handle custom heights :/
+			float height = Alert.Height;
+			Rect rect = new Rect((float)UI.screenWidth - Padding - height, topY, height, height);
+			GUI.color = Color.white;
+			GUI.DrawTexture(rect, TexGearUpAndGo.guagIconActive);
+			if (Mouse.IsOver(rect))
+			{
+				GUI.DrawTexture(rect, TexUI.HighlightTex);
+			}
+			if (Widgets.ButtonInvisible(rect, false))
+			{
+				Current.Game.GetComponent<GearUpPolicyComp>().Revert();
+			}
+			return rect;
+		}
+	}
+}
