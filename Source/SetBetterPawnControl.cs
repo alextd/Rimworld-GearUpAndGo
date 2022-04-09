bb2dc59bb03ee733fc10316e59bf1a6532133111
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-04-08 17:26:21 -0700

    Rethinking reflection access to BCP.
    Publicize BCP dll for ease of use. (https://github.com/CabbageCrow/AssemblyPublicizer)
    
    Wrap any method using it in try/catch and abort future attempts on fail.

--- Source/SetBetterPawnControl.cs
+++ Source/SetBetterPawnControl.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Reflection;
 using Verse;
 using RimWorld;
-using BetterPawnControl;
 using HarmonyLib;
 
 namespace GearUpAndGo
@@ -13,117 +12,94 @@ namespace GearUpAndGo
 	[StaticConstructorOnStartup]
 	static class SetBetterPawnControl
 	{
+		public static bool working;
+
 		public static bool Active()
 		{
-			return ModsConfig.ActiveModsInLoadOrder.Any(m => m.Name == "Better Pawn Control");
+			return ModsConfig.ActiveModsInLoadOrder.Any(m => m.PackageIdPlayerFacing == "VouLT.BetterPawnControl");
 		}
 
-		static bool warned = false;
-		//Okay, I want it really bad, so let's do this.
+		static SetBetterPawnControl()
+		{
+			Log.Message("Checking for BCP");
+			if (Active())
+			{
+				working = true;//assume it works till it fails
+				Log.Message("Using BCP");
+			}
+		}
+		public static void Stop(Exception e)
+		{
+			working = false;
+
+			Verse.Log.Error($"Hey, dang, Gear Up and Go failed to use Better Pawn Control:\n\n{e}");
+		}
+
 		public static void SetPawnControlPolicy(string policyName)
 		{
-			if (!Active()) return;
+			if (!working) return;
 
 			try
 			{
 				SetPawnControlPolicyEx(policyName);
 			}
-			catch(Exception e)
+			catch (Exception e)
 			{
-				if (!warned)
-				{
-					string desc = "Gear Up And Go Failed to set Better Pawn Control Policy, I have no idea why, probably needs a re-compile, so check for an update or tell me (sorry this is really confusing.)";
-					Find.LetterStack.ReceiveLetter("Gear+Go failed to set Pawn Control", desc, LetterDefOf.NegativeEvent, e.ToStringSafe());
-					Verse.Log.Error($"{desc}\n\n{e.ToStringSafe()}");
-					warned = true;
-				}
+				Stop(e);
 			}
 		}
 
-
-		static Type assignManager;
-		static Type policyType;
-		static FieldInfo policiesInfo;
-
-		static MethodInfo LoadStateInfo;
-		public static Delegate LoadStateDelegate;
-		static SetBetterPawnControl()
+		private static void SetPawnControlPolicyEx(string policyName)
 		{
-			assignManager = AccessTools.TypeByName("BetterPawnControl.AssignManager");
-			Log.Message($"BCP assignManager: {assignManager}");
-			policyType = AccessTools.TypeByName("BetterPawnControl.Policy");
-			Log.Message($"BCP policyType: {policyType}");
-			policiesInfo = AccessTools.Field(assignManager, "policies");
-			Log.Message($"BCP policiesInfo: {policiesInfo}");
+			BetterPawnControl.Policy policy = BetterPawnControl.AssignManager.policies.FirstOrDefault(p => p.label == policyName);
 
-			LoadStateInfo = AccessTools.Method(assignManager, "LoadState", new Type[] { policyType });
-			Log.Message($"BCP LoadStateInfo: {LoadStateInfo}");
-			LoadStateDelegate = LoadStateInfo.CreateDelegate(typeof(Action<>).MakeGenericType(policyType));
-			Log.Message($"BCP LoadStateDelegate: {LoadStateDelegate}");
-		}
-		public static void SetPawnControlPolicyEx(string policyName)
-		{ 
-			List<Policy> assignPolicies = (List<Policy>)policiesInfo.GetValue(default(object));
-			Log.Message($"assignPolicies are: {assignPolicies.ToStringSafeEnumerable()}");
-			Policy policy = assignPolicies.FirstOrDefault(p => p.label == policyName);
-			
 			if (policy != null)
 			{
 				Log.Message($"using policy: {policy}");
-				//MainTabWindow_Assign_Policies
-				//private static void LoadState(Policy policy)
-				LoadStateDelegate.DynamicInvoke(policy);
+				BetterPawnControl.AssignManager.LoadState(policy);
 			}
 		}
 
 		public static string CurrentPolicy()
 		{
-			if (!Active()) return "";
+			if (!working) return "";
 
 			try
 			{
 				return CurrentPolicyEx();
 			}
-			catch (Exception)
+			catch (Exception e)
 			{
+				Stop(e);
 				return "";
 			}
 		}
 
-		static MethodInfo GetActivePolicyInfo = AccessTools.Method(AccessTools.TypeByName("AssignManager"), "GetActivePolicy", new Type[] { });
-		public static string CurrentPolicyEx()
-		{ 
-			Log.Message($"Resetting policies");
-
-			return (GetActivePolicyInfo.Invoke(null, new object[] { }) as Policy)?.label ?? "";
-		}
+		private static string CurrentPolicyEx() =>
+			BetterPawnControl.AssignManager.GetActivePolicy()?.label ?? "";
 
 		public static List<string> PolicyList()
 		{
-			if (!Active()) return null;
+			if (!working) return null;
 
 			try
 			{
-				return PolicyListEx();
+				return PolicyListEx().ToList();
 			}
-			catch (Exception)
+			catch (Exception e)
 			{
+				Stop(e);
 				return null;
 			}
 		}
-		
-		public static List<string> PolicyListEx()
-		{
-			//Hitting can't load type errors if I make this an easier yield return, so messy lists it is
-			List<string> policyNames = new List<string>();
-			List<Policy> assignPolicies = (List<Policy>)policiesInfo.GetValue(default(object));
-			Log.Message($"assignPolicies are: {assignPolicies.ToStringSafeEnumerable()}");
-			foreach (Policy p in assignPolicies)
-			{
-				policyNames.Add(p.label);
-			}
 
-			return policyNames;
+		private static IEnumerable<string> PolicyListEx()
+		{
+			//This oneliner apparently causes problems on load when BCP not active.?
+			//BetterPawnControl.AssignManager.policies.Select(p => p.label);
+
+			foreach (BetterPawnControl.Policy p in BetterPawnControl.AssignManager.policies)
+				yield return p.label;
 		}
 	}
 }
